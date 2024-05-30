6a4ffc0ee27e27659a0ad96335902c7c52d2afc8
Shivam Chauhan <shivamchauhan2463@gmail.com>
2024-05-30 12:15:27 +0530

    filters

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+
+const links = ["all", "Next 14", "frontend", "backend", "fullstack"];
 
 const Filters = () => {
-  return <div>Filters</div>;
+  const [active, setActive] = useState("");
+
+  const handleFilter = (link: string) => {
+    setActive(link);
+  };
+
+  return (
+    <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
+      {links.map((link) => (
+        <button
+          key={link}
+          onClick={() => handleFilter(link)}
+          className={`${active === link ? "gradient_blue-purple" : ""} whitespace-nowrap rounded-lg px-8 py-2.5 capitalize`}
+        >
+          {link}
+        </button>
+      ))}
+    </ul>
+  );
 };
 
 export default Filters;
