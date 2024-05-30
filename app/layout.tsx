309184102ebfdd5f48fcdcfdc9effca9ddc610ce
Shivam Chauhan <shivamchauhan2463@gmail.com>
2024-05-30 19:21:33 +0530

    further improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "JS Mastery",
   description: "JS Mastery Resources",
+  other: {
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "twitter:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "og:url": "jsmastery.pro",
+    "og:image": "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg",
+    "og:type": "website",
+  },
 };
 
 export default function RootLayout({
